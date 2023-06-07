071f14b20a6378bb9e7bb2a547c445af2611e1b3
Cassia Moraes <ccassiamoraess@gmail.com>
2023-06-07 16:47:49 -0300

    Update Lista.js

--- src/componentes/Lista.js
+++ src/componentes/Lista.js
@@ -3,9 +3,18 @@ import React from 'react'
 function Lista(props){
    
     return(
-        <div>
+        <div>        
+            <input 
+            id={props.id}
+            type='checkbox'
+            defaultChecked={props.completed}
+        />
+        <label>
             {props.nome}
+        </label>
+    
         </div>
+        
     )
     
   }
